c05a618b8759c871e273136c8f4a464672b25b0b
karthick-krtk <karthick00642@gmail.com>
2026-06-29 20:24:45 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+int main()
+{
+    char s[100];
+    scanf("%s",s);
+    char stk[100];
+    int top=-1,f=1;
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+            stk[++top]=s[i];
+        }
+        else{
+            if(s[i]==')'){
+                if(stk[top]!='('){
+                    f=0;
+                    break;
+                }
+                else{
+                    top--;
+                }
+            }
+            else if(s[i]==']'){
+                if(stk[top]!='['){
+                    f=0;
+                    break;
+                }
+                else{
+                    top--;
+                }
+            }
+            else if(s[i]=='}'){
+                if(stk[top]!='{'){
+                    f=0;
+                    break;
+                }
+                else{
+                    top--;
+                }
+            }
+        }
+    }
+    if(f==1 && top==-1)
+        printf("Balanced");
+    else{
+        printf("Unbalanced");
+    }
+   
+}
